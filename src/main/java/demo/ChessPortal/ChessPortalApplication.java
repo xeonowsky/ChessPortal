c0efa53996cc9d26526d599942156d3182d7dd28
xeonowsky <klaudiusz599@gmail.com>
2024-08-24 19:04:06 +0200

    First Commit

---
+++ src/main/java/demo/ChessPortal/ChessPortalApplication.java
@@ -0,0 +1,13 @@
+package demo.ChessPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/ChessPortal/ChessPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.ChessPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
